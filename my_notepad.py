757e93e4e294b626db23288d4cc86c325a9ffdc4
gouthamrajesh <gouthamkannus10@gmail.com>
2021-06-03 10:50:36 +0530

    Buttons and Writing area added
    Button and Text area added, not alligned

--- my_notepad.py
+++ my_notepad.py
@@ -6,5 +6,18 @@ gou.title("Notepad Clone")
 gou.rowconfigure(0, minsize=540)
 gou.columnconfigure(1, minsize=540)
 
+text_edit= tk.Text(gou)
+text_edit.grid(row=0, column=1)
+
+frame_button= tk.Frame(gou, relief=tk.RAISED, bd=4)
+frame_button.grid(row=0, column=0)
+
+button_open= tk.Button(frame_button, text="Open File")
+button_open.grid(row=0, column=0, padx=5, pady=5)
+
+button_save= tk.Button(frame_button, text="Save As")
+button_save.grid(row=1, column=0, padx=5)
+
+
 
 gou.mainloop()
