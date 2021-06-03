9c8e021471e6ae751b1e0bca2663069e780eb0aa
gouthamrajesh <gouthamkannus10@gmail.com>
2021-06-03 10:35:28 +0530

    Notepad opened in powershell
    Notepad opened in Powershell

---
+++ my_notepad.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+
+gou=tk.Tk()
+gou.title("Notepad Clone")
+gou.rowconfigure(0, minsize=540)
+gou.columnconfigure(1, minsize=540)
+
+
+gou.mainloop()
